77656017f017b370bb373c55836fd786a00bc609
roynchan <90730734+roynchan@users.noreply.github.com>
2022-10-15 14:50:04 -0400

    updating js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // imports always go at the top of the file
 // this is called an IIFE (immediately invoked function expression)
-import { getData } from "./modules/dataMiner.js";
+import { getData1,getData2 } from "./modules/dataMiner.js";
 
 // it's a pretty common JavaScript programming pattern
 // also called a module file
@@ -10,19 +10,26 @@ import { getData } from "./modules/dataMiner.js";
     let theThings = document.querySelector('#team-section'),
         theTemplate = document.querySelector('#bio-template').content,
         lightbox = document.querySelector(".lightbox"),
-        openclick = document.querySelectorAll(".topic"),
+        closebut = document.querySelector(".button"),
+        openFootball = document.querySelector(".topicFootball"),
+        openFood = document.querySelector(".topicFood"),
+        openCountry = document.querySelector(".topicCountry"),
         faveData;
 
-       
+    function close(){
+        window.location.reload();
+    }
   
     function popup(){
        
-            lightbox.classList.toggle("shown")}
+            lightbox.classList.add("shown")}
      
 
     function buildThings(data) {
         // get all the keys (names) from the data object and use that to iterate through the data
       //debugger;
+
+     
       
         faveData = data
         
@@ -36,25 +43,65 @@ import { getData } from "./modules/dataMiner.js";
             let containers = panel.firstElementChild.children;
 
             // grab the image from the object and set it as the source 
+           
             containers[0].querySelector('img').src = `images/${data[thing].avatar}`;
             containers[0].querySelector('img').id = thing;
             containers[0].querySelector('img').addEventListener('click',showThing);
-
-            containers[1].textContent = data[thing].name,
-            containers[2].textContent = data[thing].role;
            
+      
+            containers[1].textContent = data[thing].name,
+            containers[2].textContent = data[thing].desc;
+
+            
+            
 
             theThings.appendChild(panel);
+            
+
         })
     }
 
     function showThing() {
+
         
         let currentThing = faveData[this.id];
+       
     }
     
-    getData(`./data.json`, buildThings);
-    openclick.forEach(addEventListener("click",popup));
+ 
+  function addFootball (){
+
+    
+   
+    getData1(`./data.json`,buildThings);
+    
+  }
+  function addFood (){
+
+    getData1(`./data2.json`,buildThings);
+    
+  }
+
+function addCountry (){
+    getData1(`./data3.json`,buildThings)
+}
+
+
+
+  openFootball.addEventListener("click",addFootball);
+    openFootball.addEventListener("click",popup);
+
+ 
+    openFood.addEventListener("click",addFood);
+    openFood.addEventListener("click",popup);
+
+
+    openCountry.addEventListener("click",addCountry);
+    openCountry.addEventListener("click",popup);
+  
+    
+    closebut.addEventListener("click",close);
+
    
     
 })();
